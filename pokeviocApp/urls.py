6d365df32b10be46e74cdecc869394debe188dd5
NyckolasP <nicolas@allforweb.fr>
2023-06-29 16:08:30 +0200

    design home + contact

--- pokeviocApp/urls.py
+++ pokeviocApp/urls.py
@@ -5,4 +5,5 @@ urlpatterns =[
     path('', views.about, name='about'),
     path('contact', views.contact, name='contact'),
     path('services/list', views.services_list, name='services_list'),
+    path('center', views.center, name='center'),
     ]
